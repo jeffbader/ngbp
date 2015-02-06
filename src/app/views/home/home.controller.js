320be4c0829b81ccd3763b55e01a2a6d970b816c
jeffbader <redabaj@gmail.com>
2015-02-06 09:00:34 -0800

    Removed the boiler plate text from the home page.

--- src/app/views/home/home.controller.js
+++ src/app/views/home/home.controller.js
@@ -2,10 +2,11 @@
 	'use strict';
 
 	angular
-		.module( 'app.views.home.controller', [])
+		.module( 'app.views.home.controller', ['app.config'])
 		.controller( 'HomeController', HomeController);
 		
-		function HomeController( ) {
+		function HomeController(appName ) {
+            this.appName = appName;
 		}
 		
 })();
